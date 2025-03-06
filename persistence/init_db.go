a3019f17496adde43fc9faad82ac30c978954248
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-03-06 21:15:09 +0200

    add logs

--- persistence/init_db.go
+++ persistence/init_db.go
@@ -16,19 +16,20 @@ func NewClientRepository(client driver.Client) *ClientRepository {
 }
 
 func (cr ClientRepository) GetOrCreateDB(dbName string) (driver.Database, error) {
-	db_exists, err := cr.client.DatabaseExists(context.Background(), dbName)
+	dbExists, err := cr.client.DatabaseExists(context.Background(), dbName)
 	if err != nil {
 		log.Printf("Failed to check if database exists")
-
 	}
 	var db driver.Database
-	if !db_exists {
+	if !dbExists {
+		log.Printf("Database doen't exist")
 		db, err = cr.client.CreateDatabase(context.Background(), dbName, nil)
 		if err != nil {
 			log.Printf("Failed to create database")
 			return nil, err
 		}
 	} else {
+		log.Printf("Database already exists")
 		db, err = cr.client.Database(context.Background(), dbName)
 		if err != nil {
 			log.Printf("Failed to open database")
