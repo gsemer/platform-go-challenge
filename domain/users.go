8994d4ebb1a73f0078d11d85b43821f1e3009b51
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-03-07 18:11:44 +0200

    fix key name

--- domain/users.go
+++ domain/users.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	Key       string    `json:"_key,omitempty"`
 	FirstName string    `json:"first_name"`
-	LatName   string    `json:"last_name"`
+	LastName  string    `json:"last_name"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
