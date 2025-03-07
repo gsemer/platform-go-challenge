00797a0a320ddcb61edf6bab3b7554745ec072b8
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-03-07 17:56:03 +0200

    variables for env vars

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/arangodb/go-driver"
-	"github.com/arangodb/go-driver/http"
+	arangohttp "github.com/arangodb/go-driver/http"
 )
 
 func getEnv(key, fallback string) string {
@@ -23,9 +23,14 @@ func main() {
 	log.Println("Waiting 10 seconds for the ArangoDB container to be ready...")
 	time.Sleep(10 * time.Second)
 
-	connection, err := http.NewConnection(
-		http.ConnectionConfig{
-			Endpoints: []string{getEnv("ARANGO_URL", "")},
+	arangoURL := getEnv("ARANGO_URL", "")
+	arangoUsername := getEnv("ARANGO_USERNAME", "root")
+	arangoPassword := getEnv("ARANGO_PASSWORD", "")
+	arangoDB := getEnv("ARANGO_DB", "")
+
+	connection, err := arangohttp.NewConnection(
+		arangohttp.ConnectionConfig{
+			Endpoints: []string{arangoURL},
 		},
 	)
 	if err != nil {
@@ -34,25 +39,37 @@ func main() {
 
 	client, err := driver.NewClient(
 		driver.ClientConfig{
-			Connection: connection,
-			Authentication: driver.BasicAuthentication(
-				getEnv("ARANGO_USERNAME", "root"),
-				getEnv("ARANGO_PASSWORD", ""),
-			),
+			Connection:     connection,
+			Authentication: driver.BasicAuthentication(arangoUsername, arangoPassword),
 		},
 	)
 	if err != nil {
 		log.Fatalf("Failed to create the client: %v", err)
 	}
 
-	_, err = client.Database(context.Background(), getEnv("ARANGO_DB", ""))
+	db, err := client.Database(context.Background(), arangoDB)
 	if err != nil {
+		log.Printf("Error getting database %s: %v", arangoDB, err)
 		if driver.IsNotFoundGeneral(err) {
-			_, err = client.CreateDatabase(context.Background(), "platform-db", nil)
+			db, err = client.CreateDatabase(context.Background(), arangoDB, nil)
 			if err != nil {
 				log.Fatalf("Failed to create database: %v", err)
 			}
 		}
 		log.Println("Database 'platform-db' was created successfully")
+	}
+
+	collectionNames := []string{"user", "asset", "favourite"}
+	for _, collectionName := range collectionNames {
+		_, err := db.Collection(context.Background(), collectionName)
+		if err != nil {
+			if driver.IsNotFoundGeneral(err) {
+				_, err = db.CreateCollection(context.Background(), collectionName, nil)
+				if err != nil {
+					log.Fatalf("Failed to create collection: %v", err)
+				}
+			}
+			log.Printf("Collection %s was created successfully", collectionName)
+		}
 	}
 }
