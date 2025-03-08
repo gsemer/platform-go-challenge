b9770ba74b7c6768450ac1d4cbe24607207bfce7
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-03-08 21:11:45 +0200

    unit tests

---
+++ app/services/favourites_test.go
@@ -0,0 +1,50 @@
+package services
+
+import (
+	"errors"
+	"platform-go-challenge/domain"
+	"platform-go-challenge/persistence/fakes"
+	"testing"
+	"time"
+)
+
+func TestAddToFavourites(t *testing.T) {
+	fr := &fakes.FakeFavouriteRepository{}
+
+	favourite := domain.Favourite{
+		From:      "user/1",
+		To:        "asset/2",
+		CreatedAt: time.Now(),
+	}
+	fr.AddToFavouritesReturns(favourite, nil)
+
+	favouriteService := FavouriteService{fr: fr}
+
+	actual, err := favouriteService.AddToFavourites("1", "2")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if actual.From != favourite.From {
+		t.Errorf("It was expected to have %v as output,\n but we got %v instead", favourite.From, actual.From)
+	}
+	if actual.To != favourite.To {
+		t.Errorf("It was expected to have %v as output,\n but we got %v instead", favourite.To, actual.To)
+	}
+}
+
+func TestAddToFavourites_FAIL(t *testing.T) {
+	fr := &fakes.FakeFavouriteRepository{}
+
+	favourite := domain.Favourite{}
+	fr.AddToFavouritesReturns(favourite, errors.New("document not found"))
+
+	favouriteService := FavouriteService{fr: fr}
+
+	_, err := favouriteService.AddToFavourites("1", "2")
+
+	if err.Error() != "document not found" {
+		t.Errorf("Expected document not found error, got %v instead", err)
+	}
+}
