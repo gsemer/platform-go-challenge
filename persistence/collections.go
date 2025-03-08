7e61a134541c7b5abb86b97dade88169e2ef5db4
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-03-08 20:04:58 +0200

    distinguish between document and edge collection

--- persistence/collections.go
+++ persistence/collections.go
@@ -15,13 +15,26 @@ func NewCollectionRepository(db driver.Database) *CollectionRepository {
 	return &CollectionRepository{db: db}
 }
 
-func (cr CollectionRepository) GetOrCreate(name string) driver.Collection {
+func (cr CollectionRepository) GetOrCreate(name string, _type string) driver.Collection {
 	collection, err := cr.db.Collection(context.Background(), name)
 	if err != nil {
 		if driver.IsNotFoundGeneral(err) {
-			collection, err = cr.db.CreateCollection(context.Background(), name, nil)
-			if err != nil {
-				log.Fatalf("Failed to create collection: %v", err)
+			if _type == "document" {
+				collection, err = cr.db.CreateCollection(context.Background(), name, &driver.CreateCollectionOptions{
+					Type: driver.CollectionTypeDocument,
+				})
+				if err != nil {
+					log.Fatalf("Failed to create document collection: %v", err)
+				}
+			} else if _type == "edge" {
+				collection, err = cr.db.CreateCollection(context.Background(), name, &driver.CreateCollectionOptions{
+					Type: driver.CollectionTypeEdge,
+				})
+				if err != nil {
+					log.Fatalf("Failed to create edge collection: %v", err)
+				}
+			} else {
+				log.Fatalf("Unknown type of collection")
 			}
 		}
 		log.Printf("Collection %s was created successfully", name)
